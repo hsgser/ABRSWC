c71cd47fec7b4062fec8f2419cf44fffbf8ac108
hsgser <s1f101701171@iniad.org>
2020-12-03 23:47:52 +0900

    Added notebooks to evalute experiment results

--- experiments.py
+++ experiments.py
@@ -10,8 +10,8 @@ print(type(G), len(G.nodes()), len(G.edges())) # debug
 print(pdist.shape) # debug
 
 # Compare between methods
-# run_time, travel_cost = run_experiment1(G, pdist, repeats=10)
-# save_experiment_results(data_dir, 'compare_methods', run_time, travel_cost)
+run_time, travel_cost = run_experiment1(G, pdist, repeats=10)
+save_experiment_results(data_dir, 'compare_methods', run_time, travel_cost)
 
 # Compare group size
 sizes       = np.arange(4, 18, step=2)
@@ -22,41 +22,41 @@ run_time, travel_cost = run_experiment2(G, pdist, param='S', S=sizes, run_optima
 save_experiment_results(data_dir, 'compare_group_size', run_time, travel_cost)
 
 # Compare number of users
-# sizes       = np.arange(8, 101, step=8)
-# run_time    = []    
-# travel_cost = []
+sizes       = np.arange(8, 101, step=8)
+run_time    = []    
+travel_cost = []
 
-# for N_u in sizes:
-#     print(f"N_u = {N_u}")
-#     _time, _cost = run_experiment1(G, pdist, N_u=N_u, run_optimal=False)
-#     # Store the mean of all runs
-#     _time = _time.mean(axis=0)
-#     _cost = _cost.mean(axis=0)
-#     run_time.extend(_time)
-#     travel_cost.extend(_cost)
+for N_u in sizes:
+    print(f"N_u = {N_u}")
+    _time, _cost = run_experiment1(G, pdist, N_u=N_u, run_optimal=False)
+    # Store the mean of all runs
+    _time = _time.mean(axis=0)
+    _cost = _cost.mean(axis=0)
+    run_time.extend(_time)
+    travel_cost.extend(_cost)
 
-# save_experiment_results(data_dir, 'compare_number_of_user', run_time, travel_cost)
+save_experiment_results(data_dir, 'compare_number_of_user', run_time, travel_cost)
 
 # Compare number of POIs
-# sizes       = np.arange(10, 101, step=10)
-# run_time    = []    
-# travel_cost = []
+sizes       = np.arange(10, 101, step=10)
+run_time    = []    
+travel_cost = []
 
-# for N_p in sizes:
-#     print(f"N_p = {N_p}")
-#     _time, _cost = run_experiment1(G, pdist, N_p=N_p, run_optimal=False)
-#     # Store the mean of all runs
-#     _time = _time.mean(axis=0)
-#     _cost = _cost.mean(axis=0)
-#     run_time.extend(_time)
-#     travel_cost.extend(_cost)
+for N_p in sizes:
+    print(f"N_p = {N_p}")
+    _time, _cost = run_experiment1(G, pdist, N_p=N_p, run_optimal=False)
+    # Store the mean of all runs
+    _time = _time.mean(axis=0)
+    _cost = _cost.mean(axis=0)
+    run_time.extend(_time)
+    travel_cost.extend(_cost)
 
-# save_experiment_results(data_dir, 'compare_number_of_poi', run_time, travel_cost)
+save_experiment_results(data_dir, 'compare_number_of_poi', run_time, travel_cost)
 
 # Compare extra ratio
-# sizes       = np.linspace(1, 2, num=11)
-# run_time    = []    
-# travel_cost = []
+sizes       = np.linspace(1, 2, num=11)
+run_time    = []    
+travel_cost = []
 
-# run_time, travel_cost = run_experiment2(G, pdist, param='e', epsilon=sizes, run_optimal=False)
-# save_experiment_results(data_dir, 'compare_extra_ratio', run_time, travel_cost)
+run_time, travel_cost = run_experiment2(G, pdist, param='e', epsilon=sizes, run_optimal=False)
+save_experiment_results(data_dir, 'compare_extra_ratio', run_time, travel_cost)
