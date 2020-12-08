9c8bdbb99f10b878252e123d2934509cfd388437
hsgser <s1f101701171@iniad.org>
2020-12-09 01:33:25 +0900

    Modified figures in experiments

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import numpy as np
 import networkx as nx
 from algorithms import *
+import matplotlib.pyplot as plt
 
 def load_data(data_dir):
     """
@@ -77,7 +78,7 @@ def run_experiment1(G, pdist, repeats=10, z=4, N_u=16, N_p=20, run_optimal=True)
         Number of users.
     N_p: int, default = 20
         Number of POIs.
-    run_optimal: bool, default=True
+    run_optimal: bool, default = True
         If true, run the optimal solution. Otherwise, do not run the optimal solution.
     
     Returns
@@ -178,7 +179,7 @@ def run_experiment2(G, pdist, param, repeats=10, z=4, epsilon=1.5, S=8, run_opti
         Extra ratio.
     S: int or array-like, default = 8
         Group size.
-    run_optimal: bool, default=True
+    run_optimal: bool, default = True
         If true, run the optimal solution. Otherwise, do not run the optimal solution.
     
     Returns
@@ -274,3 +275,168 @@ def run_experiment2(G, pdist, param, repeats=10, z=4, epsilon=1.5, S=8, run_opti
     travel_cost = np.array(travel_cost).mean(axis=0)
 
     return run_time, travel_cost
+
+def plot_proc_time(df, X, exp_name, ylim, fig_dir='data/figure', xlabel='Run', plot_optimal=False, legend_loc=(0, 0.5)):
+    """
+    An utility function to plot the mean processing time.
+
+    Parameters
+    ----------
+    df: DataFrame
+        A DataFrame contains the mean processing time.
+    X: array
+        Horizontal coordinates of data.
+    exp_name: string
+        Name of the experiment.
+    ylim: array, len = 2
+        y-axis limits.
+    fig_dir: path-like object, default = 'data/figure'
+        Path of figure folder.
+    xlabel: string, default = 'Run'
+        x-axis label.
+    plot_optimal: bool, default = False
+        If true, plot the optimal solution. Otherwise, do not plot the optimal solution.
+    legend_loc: array, len = 2, default = (0, 0.5)
+        The location of the legend
+    """
+    _, ax = plt.subplots(figsize=(10, 7))
+    if plot_optimal:
+        ax.plot(X, df['optimal'], marker='o', markersize=8, color='blue', label='optimal')
+    ax.plot(X, df['approx'], marker='d',  markersize=8, color='orange', label='approx')
+    ax.plot(X, df['approx_woc'], marker='s',  markersize=8, color='green', label='approx_woc')
+    ax.set_yscale('log')
+    ax.set_ylim(*ylim)
+    ax.tick_params(axis='both', labelsize=20)
+    ax.set_xlabel(xlabel, fontsize=30)
+    ax.set_ylabel('Mean processing time [s]', fontsize=30)
+    ax.grid(True, axis='y')
+    ax.spines['bottom'].set_alpha(0.4)
+    ax.spines['top'].set_alpha(0.4)
+    ax.spines['left'].set_alpha(0.4)
+    ax.spines['right'].set_alpha(0.4)
+    ax.legend(loc=legend_loc, framealpha=0.0, fontsize=20)
+    plt.savefig(os.path.join(fig_dir, f'{exp_name}_proc_time.png'), bbox_inches='tight')
+    plt.show()
+
+def plot_travel_cost(df, X, exp_name, ylim, fig_dir='data/figure', xlabel='Run', plot_optimal=False, legend_loc=(0.5, 0.69)):
+    """
+    An utility function to plot the total travel cost.
+
+    Parameters
+    ----------
+    df: DataFrame
+        A DataFrame contains the total travel cost.
+    X: array
+        Horizontal coordinates of data.
+    exp_name: string
+        Name of the experiment.
+    ylim: array, len = 2
+        y-axis limits.
+    fig_dir: path-like object, default = 'data/figure'
+        Path of figure folder.
+    xlabel: string, default = 'Run'
+        x-axis label.
+    plot_optimal: bool, default = False
+        If true, plot the optimal solution. Otherwise, do not plot the optimal solution.
+    legend_loc: array, len = 2, default = (0.5, 0.69)
+        The location of the legend
+    """
+    _, ax = plt.subplots(figsize=(10, 7))
+    if plot_optimal:
+        ax.plot(X, df['optimal'], marker='o', markersize=8, color='blue', label='optimal')
+    ax.plot(X, df['approx'], marker='d', markersize=8, color='orange', label='approx')
+    ax.plot(X, df['approx_woc'], marker='s', markersize=8, color='green', label='approx_woc')
+    ax.plot(X, df['greedy'], marker='v', markersize=8, color='red', label='greedy')
+    ax.set_ylim(*ylim)
+    ax.tick_params(axis='both', labelsize=20)
+    ax.set_xlabel(xlabel, fontsize=30)
+    ax.set_ylabel('Total travel cost [m]', fontsize=30)
+    ax.grid(True, axis='y')
+    ax.spines['bottom'].set_alpha(0.4)
+    ax.spines['top'].set_alpha(0.4)
+    ax.spines['left'].set_alpha(0.4)
+    ax.spines['right'].set_alpha(0.4)
+    ax.legend(loc=legend_loc, framealpha=0.0, fontsize=20)
+    plt.savefig(os.path.join(fig_dir, f'{exp_name}_travel_cost.png'), bbox_inches='tight')
+    plt.show()
+
+def plot_approx_ratio(df, X, exp_name, ylim, fig_dir='data/figure', xlabel='Run', legend_loc=(0.3, 0.63)):
+    """
+    An utility function to plot the approximation ratio.
+
+    Parameters
+    ----------
+    df: DataFrame
+        A DataFrame contains the approximation ratio.
+    X: array
+        Horizontal coordinates of data.
+    exp_name: string
+        Name of the experiment.
+    ylim: array, len = 2
+        y-axis limits.
+    fig_dir: path-like object, default = 'data/figure'
+        Path of figure folder.
+    xlabel: string, default = 'Run'
+        x-axis label.
+    plot_optimal: bool, default = False
+        If true, plot the optimal solution. Otherwise, do not plot the optimal solution.
+    legend_loc: array, len = 2, default = (0.3, 0.63)
+        The location of the legend
+    """
+    _, ax = plt.subplots(figsize=(10, 7))
+    ax.plot(X, df['approx_ratio'], marker='d', markersize=8, color='orange', label='approx')
+    ax.plot(X, df['approx_woc_ratio'], marker='s', markersize=8, color='green', label='approx_woc')
+    ax.plot(X, df['greedy_ratio'],  marker='v', markersize=8, color='red', label='greedy')
+    ax.set_ylim(*ylim)
+    ax.tick_params(axis='both', labelsize=20)
+    ax.set_xlabel(xlabel, fontsize=30)
+    ax.set_ylabel('Approximation ratio', fontsize=30)
+    ax.grid(True, axis='y')
+    ax.spines['bottom'].set_alpha(0.4)
+    ax.spines['top'].set_alpha(0.4)
+    ax.spines['left'].set_alpha(0.4)
+    ax.spines['right'].set_alpha(0.4)
+    ax.legend(loc=legend_loc, framealpha=0.0, fontsize=20)
+    plt.savefig(os.path.join(fig_dir, f'{exp_name}_approx_ratio.png'), bbox_inches='tight')
+    plt.show()
+
+def plot_rs_efficiency(df, X, exp_name, ylim, fig_dir='data/figure', xlabel='Run', plot_optimal=False, legend_loc=(0.3, 0.63)):
+    """
+    An utility function to plot the ride-sharing efficiency.
+
+    Parameters
+    ----------
+    df: DataFrame
+        A DataFrame contains the ride-sharing efficiency.
+    X: array
+        Horizontal coordinates of data.
+    exp_name: string
+        Name of the experiment.
+    ylim: array, len = 2
+        y-axis limits.
+    fig_dir: path-like object, default = 'data/figure'
+        Path of figure folder.
+    xlabel: string, default = 'Run'
+        x-axis label.
+    plot_optimal: bool, default = False
+        If true, plot the optimal solution. Otherwise, do not plot the optimal solution.
+    legend_loc: array, len = 2, default = (0.5, 0.63)
+        The location of the legend
+    """
+    _, ax = plt.subplots(figsize=(10, 7))
+    if plot_optimal:
+        ax.plot(X, df['optimal_gain'], marker='o', markersize=8, color='blue', label='optimal')
+    ax.plot(X, df['approx_gain'], marker='d', markersize=8, color='orange', label='approx')
+    ax.plot(X, df['approx_woc_gain'], marker='s', markersize=8, color='green', label='approx_woc')
+    ax.set_ylim(*ylim)
+    ax.tick_params(axis='both', labelsize=20)
+    ax.set_xlabel(xlabel, fontsize=30)
+    ax.set_ylabel('Ride-sharing efficiency', fontsize=30)
+    ax.grid(True, axis='y')
+    ax.spines['bottom'].set_alpha(0.4)
+    ax.spines['top'].set_alpha(0.4)
+    ax.spines['left'].set_alpha(0.4)
+    ax.spines['right'].set_alpha(0.4)
+    ax.legend(loc=legend_loc, framealpha=0.0, fontsize=20)
+    plt.savefig(os.path.join(fig_dir, f'{exp_name}_rs_efficiency.png'), bbox_inches='tight')
+    plt.show()
